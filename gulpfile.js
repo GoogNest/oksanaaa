efe6708ce7f12b1522da380a144bc7e7513747f8
Doofyz <blablacasino@gmail.com>
2020-08-20 15:14:42 +0000

    1 step

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+
+gulp.task('watch', function() {
+
+    browserSync.init({
+        server: "./source"
+    });
+
+    gulp.watch('./source/scss/**/*.scss', gulp.series('sass-compile'));
+    gulp.watch('./source/scss/**/*.scss').on('change', browserSync.reload);
+    gulp.watch('./source/**/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('sass-compile', function() {
+
+    return gulp.src('./source/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./source/css/'))
+    .pipe(browserSync.stream());
+})
