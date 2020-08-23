bb38b8d465dd0bedd5ce6c18bc2c6ba1fa09bfd4
Doofyz <blablacasino@gmail.com>
2020-08-23 19:56:10 +0300

    modal buy done

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,18 @@
 var btn_details__advanced = document.querySelector(".advanced-learn__details");
 var mod_details__advanced = document.querySelector(".advanced-learn__modal-details");
 var close__advanced  = mod_details__advanced.querySelector(".advanced-learn__modal-details--close");
+
 var btn_details__base = document.querySelector(".base-learn__details");
 var mod_details__base = document.querySelector(".base-learn__modal-details");
 var close__base  = mod_details__base.querySelector(".base-learn__modal-details--close");
 
+var btn_details__advanced__buy = document.querySelector(".advanced-learn__modal-details__buy");
+var mod_details__advanced__buy = document.querySelector(".advanced-learn__modal-buy");
+var close__advanced__buy  = mod_details__advanced__buy.querySelector(".advanced-learn__modal-buy--close");
+
+var btn_details__base__buy = document.querySelector(".base-learn__modal-details__buy");
+var mod_details__base__buy = document.querySelector(".base-learn__modal-buy");
+var close__base__buy  = mod_details__base__buy.querySelector(".base-learn__modal-buy--close");
 
 var isStorageSupport = true;
 var storage_login = "";
@@ -20,14 +28,6 @@ try {
 btn_details__advanced.addEventListener("click", function (evt) {
     evt.preventDefault();
     mod_details__advanced.classList.add("advanced-learn__modal-details--modal-show");   
-    
-    // if (storage_login && storage_email) {
-    //     login.value = storage_login;
-    //     email.value = storage_email;
-    //     text.focus();
-    // } else {
-    //     login.focus(); 
-    // }
 });
 
 close__advanced.addEventListener("click", function (evt) {
@@ -38,22 +38,48 @@ close__advanced.addEventListener("click", function (evt) {
 
 btn_details__base.addEventListener("click", function (evt) {
     evt.preventDefault();
-    mod_details__base.classList.add("advanced-learn__modal-details--modal-show");   
-    
-    // if (storage_login && storage_email) {
+    mod_details__base.classList.add("base-learn__modal-details--modal-show");       
+});
+
+close__base.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mod_details__base.classList.remove("base-learn__modal-details--modal-show");
+    mod_details__base.classList.remove("base-learn__modal-details--modal-error");
+});
+
+btn_details__advanced__buy.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mod_details__advanced__buy.classList.add("advanced-learn__modal-buy--modal-show"); 
+    mod_details__advanced.classList.remove("advanced-learn__modal-details--modal-show");
+    mod_details__advanced.classList.remove("advanced-learn__modal-details--modal-error");      
+});
+
+close__advanced__buy.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mod_details__advanced__buy.classList.remove("advanced-learn__modal-buy--modal-show");
+    mod_details__advanced__buy.classList.remove("advanced-learn__modal-buy--modal-error");
+});
+
+btn_details__base__buy.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mod_details__base__buy.classList.add("base-learn__modal-buy--modal-show"); 
+    mod_details__base.classList.remove("base-learn__modal-details--modal-show");
+    mod_details__base.classList.remove("base-learn__modal-details--modal-error");      
+});
+
+close__base__buy.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mod_details__base__buy.classList.remove("base-learn__modal-buy--modal-show");
+    mod_details__base__buy.classList.remove("base-learn__modal-buy--modal-error");
+});
+
+// if (storage_login && storage_email) {
     //     login.value = storage_login;
     //     email.value = storage_email;
     //     text.focus();
     // } else {
     //     login.focus(); 
     // }
-});
-
-close__base.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    mod_details__base.classList.remove("advanced-learn__modal-details--modal-show");
-    mod_details__base.classList.remove("advanced-learn__modal-details--modal-error");
-});
 
 // form.addEventListener("submit", function (evt) {
     
@@ -73,12 +99,12 @@ close__base.addEventListener("click", function (evt) {
 
 
 
-window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-        if (mod_wu.classList.contains("modal-show")) {
-            mod_wu.classList.remove("modal-show");
-            mod_wu.classList.remove("modal-error");
-            localStorage.clear();
-        }
-    }
-});
+// window.addEventListener("keydown", function (evt) {
+//     if (evt.keyCode === 27) {
+//         if (mod_wu.classList.contains("modal-show")) {
+//             mod_wu.classList.remove("modal-show");
+//             mod_wu.classList.remove("modal-error");
+//             localStorage.clear();
+//         }
+//     }
+// });
